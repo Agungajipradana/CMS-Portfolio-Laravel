fe1d1b0e92e44c30a70d7f607dbf4569d9ffa0f8
unknown <agungajipradana@gmail.com>
2024-12-22 21:57:47 +0700

    Setup Bootstrap CSS

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-import './bootstrap';
+import { bootstrap, enableTooltip, enablePopover } from "./plugins/bootstrap";
+
+try {
+    enableTooltip(true);
+    enablePopover(true);
+    window.bootstrap = bootstrap;
+} catch (exception) {
+    console.error(exception);
+}
