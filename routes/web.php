f55199cf32f589797212cc5739ff1e612e9f66d8
unknown <agungajipradana@gmail.com>
2024-12-23 21:21:19 +0700

    Create Auth Layout

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\site\HomeController;
+use App\Http\Controllers\Site\HomeController;
+use App\Http\Controllers\Auth\LoginController;
 
 Route::get('/', [HomeController::class, 'index'])->name('site.home');
+Route::get('/login', [LoginController::class, 'index'])->name('auth.login');
