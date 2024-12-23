c4b87fb4fda270714ef4c2e0739a4505c6c74d2b
unknown <agungajipradana@gmail.com>
2024-12-23 22:02:05 +0700

    Design Auth Layout

--- resources/views/auth/login/index.blade.php
+++ resources/views/auth/login/index.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.auth.auth-layout')
 
+@section('title', 'Login')
+
 @section('content')
 Login Page
 @endsection
--- resources/views/layouts/auth/auth-layout.blade.php
+++ resources/views/layouts/auth/auth-layout.blade.php
@@ -1,11 +1,30 @@
+@php
+$app = [
+'name' => config('app.name'),
+'lang' => str_replace('_','-', app()->getLocale())
+]
+@endphp
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ $app['lang'] }}">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Auth Layout</title>
+    <title>{{$app['name']}} | @yield('title')</title>
+    @vite('resources/sass/app.scss')
 </head>
-<body>
-    @yield('content')
+
+<body class="bg-primary">
+    <div id="layoutAuthentication">
+        <div id="layoutAuthentication_content">
+            <main>
+                @yield('content')
+            </main>
+        </div>
+        @include('layouts.auth._footer')
+    </div>
+
+    @vite('resources/js/app.js')
 </body>
+
 </html>
