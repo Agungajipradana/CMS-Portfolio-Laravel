345de0a4b418855ff074dcff68924276f1d5f80b
unknown <agungajipradana@gmail.com>
2024-12-25 21:29:46 +0700

    Create Form Login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -7,7 +7,17 @@ use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
-    public function index(){
-        return view("auth.login.index");
+    public function index()
+    {
+        return view("auth.login.index", [
+            'routeAction' => route('auth.login.action'),
+            'routeHome' => route('site.home'),
+            'routeForgotPassword' => 'todo-forgot-password'
+        ]);
+    }
+
+    public function action(Request $request)
+    {
+        dd($request->all());
     }
 }
