512733e208076c309121c781fe76c077ee507807
unknown <agungajipradana@gmail.com>
2024-12-27 22:35:14 +0700

    Add Middleware Auth and Guest

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,14 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Dashboard\HomeController;
 
 Route::get('/', [SiteHomeController::class, 'index'])->name('site.home');
-Route::get('/login', [LoginController::class, 'index'])->name('auth.login');
-Route::post('/login', [LoginController::class, 'action'])->name('auth.login.action');
-Route::get('/dashboard', [HomeController::class, 'index'])->name('dashboard.home');
+
+// not logged in
+Route::middleware(['guest'])->group(function () {
+    Route::get('/login', [LoginController::class, 'index'])->name('auth.login');
+    Route::post('/login', [LoginController::class, 'action'])->name('auth.login.action');
+});
+
+// logged in
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', [HomeController::class, 'index'])->name('dashboard.home');
+});
