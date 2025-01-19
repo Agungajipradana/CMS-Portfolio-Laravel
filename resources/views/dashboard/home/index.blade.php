24e50e479af7bb27af70947c26e2e6f3017535f6
unknown <agungajipradana@gmail.com>
2025-01-19 22:52:47 +0700

    Dashboard Layout Design

--- resources/views/dashboard/home/index.blade.php
+++ resources/views/dashboard/home/index.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.dashboard.dashboard-layout')
 
+@section('title', 'Dashboad page')
+
 @section('content')
     Dashboard page
     <form action="{{route('auth.logout')}}" method="POST">
--- resources/views/layouts/dashboard/dashboard-layout.blade.php
+++ resources/views/layouts/dashboard/dashboard-layout.blade.php
@@ -1,11 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard Layout</title>
+    <title>|   @yield('title')</title>
+    @vite(['resources/sass/app.scss'])
 </head>
-<body>
-    @yield('content')
+
+<body class="sb-nav-fixed">
+    @include('layouts.dashboard._navbar')
+    <div id="layoutSidenav">
+        <div id="layoutSidenav_nav">
+            @include('layouts.dashboard._sidenav')
+        </div>
+        <div id="layoutSidenav_content">
+            <main>
+                <div class="container-fluid px-4">
+                    <h1 class="mt-4">
+                      @yield('title')
+                    </h1>
+                    @include('layouts.dashboard._breadcrumb')
+                    @yield('content')
+                </div>
+            </main>
+            @include('layouts.dashboard._footer')
+        </div>
+    </div>
+    @vite(['resources/js/app.js'])
 </body>
+
 </html>
